4d58730238a1da992e2201aaf5ea487432767b53
alwoodm <oleklisalwood@gmail.com>
2022-09-06 16:26:08 +0200

    Create Ćwiczenie 2.py

---
+++ 2.1 Wprowadzenie/Ćwiczenie 2.py
@@ -0,0 +1,8 @@
+import time
+# Pole trójkąta
+print("Witaj w programie do obliczania pola trójkąta")
+a = input("Wprowadź długość podstawy trójkąta: ")
+h = input("Wprowadź wysokość trójkąta: ")
+p = a * h / 2
+print("Pole twojego trójkąta wynosi", int(p))
+time.sleep(10)
