c1e9b6126d53931f5038446bfd3a80d85ae68f13
alwoodm <oleklisalwood@gmail.com>
2022-09-06 17:22:31 +0200

    .

--- 2.1 Wprowadzenie/Cwiczenie 2.py
+++ 2.1 Wprowadzenie/Cwiczenie 2.py
@@ -1,8 +1,8 @@
-import time
 # Pole trójkąta
 print("Witaj w programie do obliczania pola trójkąta")
 a = input("Wprowadź długość podstawy trójkąta: ")
 h = input("Wprowadź wysokość trójkąta: ")
 p = a * h / 2
 print("Pole twojego trójkąta wynosi", int(p))
-time.sleep(10)
+
+input("Wciśnij Enter aby zamknąć program")
---
+++ 2.1 Wprowadzenie/Cwiczenie 4.py
@@ -0,0 +1,14 @@
+#Różnice języków scratch i python
+import time
+def pauza():
+    time.sleep(3)
+
+print("Dzień dobry")
+pauza
+print("Dziś uczymy się programowania")
+pauza
+print("Dzisiaj zobaczymy podobieństwa i różnice języka python i scratch")
+pauza
+print("")
+print("Podobieństwa: Są to języky iterpretacyjne nie kompilowane, wmiare niski poziom zaawansowania")
+print()
